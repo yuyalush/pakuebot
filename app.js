efed425bee2aa86fa0b74bca310126acd540fa72
Yuya Yoshida <yoshidayuya@gmail.com>
2015-06-18 17:41:57 +0900

    fix app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
- 
+var hellobot = require('./hellobot');
+
 var app = express();
 var port = process.env.PORT || 3000;
  
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+app.post('/hello', hellobot);
  
 // error handler
 app.use(function (err, req, res, next) {
